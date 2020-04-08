4fea8d45baa881c779bc3f1a663d4b632a7f7604
Isaac van Bakel <ivb@vanbakel.io>
2020-04-08 15:23:39 +0100

    Initial commit

---
+++ Language.v
@@ -0,0 +1,242 @@
+Require Import Coq.Lists.List.
+Require Import Coq.Strings.String.
+
+Module Syntax.
+
+Inductive baseCapability : Type :=
+  | iso
+  | trn
+  | ref
+  | val
+  | box
+  | tag.
+
+Inductive capability : Type :=
+  | base : baseCapability -> capability
+  | isohat : capability
+  | trnhat : capability. 
+
+Reserved Notation "k <; k'" (at level 80).
+
+Inductive subcapability : capability -> capability -> Prop :=
+  | subcap_refl (k : capability) : k <; k
+  | subcap_trans (k k' k'' : capability) : k <; k' -> k' <; k'' -> k <; k''
+  | subcap_isohat_iso : isohat <; base iso
+  | subcap_isohat_trnhat : isohat <; trnhat
+  | subcap_trnhat_trn : trnhat <; base trn
+  | subcap_trnhat_ref : trnhat <; base ref
+  | subcap_trnhat_val : trnhat <; base val
+  | subcap_trn_box : base trn <; base box
+  | subcap_ref_box : base ref <; base box
+  | subcap_val_box : base val <; base box
+  | subcap_iso_tag : base iso <; base tag
+  | subcap_box_tag : base box <; base tag
+where "k <; k'" := (subcapability k k').
+
+Example all_subcap_tag (k : capability) : k <; base tag :=
+  match k with
+    | isohat => subcap_trans isohat (base iso) (base tag) (subcap_isohat_iso) (subcap_iso_tag)
+    | trnhat => subcap_trans trnhat (base trn) (base tag) (subcap_trnhat_trn) (subcap_trans (base trn) (base box) (base tag) (subcap_trn_box) (subcap_box_tag))
+    | base iso => subcap_iso_tag
+    | base trn => subcap_trans (base trn) (base box) (base tag) (subcap_trn_box) (subcap_box_tag)
+    | base ref => subcap_trans (base ref) (base box) (base tag) (subcap_ref_box) (subcap_box_tag)
+    | base val => subcap_trans (base val) (base box) (base tag) (subcap_val_box) (subcap_box_tag)
+    | base box => subcap_box_tag
+    | base tag => subcap_refl (base tag)
+  end.
+
+Definition alias (cap : capability) : capability :=
+  match cap with
+    | isohat => base iso
+    | trnhat => base trn
+    | base iso => base tag
+    | base trn => base box
+    | base b => base b
+  end.
+
+Inductive sendable : baseCapability -> Prop :=
+  | sendable_tag : sendable tag
+  | sendable_val : sendable val
+  | sendable_iso : sendable iso.
+
+Definition isSendable (cap : baseCapability) : bool :=
+  match cap with
+    | tag => true
+    | val => true
+    | iso => true
+    | _   => false
+  end.
+
+Inductive classId : Type :=
+  | cId : string -> classId.
+
+Inductive actorId : Type :=
+  | aId : string -> actorId.
+
+Definition typeId : Type := classId + actorId.
+
+Inductive ponyType : Type :=
+  | type (s : typeId) (k : capability).
+
+Inductive aliasedType : Type :=
+  | aType (s : typeId) (b : baseCapability).
+
+Definition asPonyType (aT : aliasedType) : ponyType :=
+  match aT with
+    | aType s b => type s (base b)
+  end.
+
+Definition hat (aT : aliasedType) : ponyType :=
+  match aT with
+    | aType s iso => type s isohat
+    | aType s trn => type s trnhat
+    | aType s b   => type s (base b)
+  end.
+
+Inductive var : Type :=
+  | variable : string -> var.
+
+Inductive Temporary : Type :=
+  | temporary : string -> Temporary.
+
+Inductive path : Type :=
+  | use (x : var)
+  | consume (x : var)
+  | temp (t : Temporary).
+
+Inductive fieldId : Type :=
+  | fId : string -> fieldId.
+
+Definition fieldOfPath : Type := prod path fieldId.
+
+Inductive methodId : Type :=
+  | mId : string -> methodId.
+
+Inductive behaviourId : Type :=
+  | bId : string -> behaviourId.
+
+Inductive constructorId : Type :=
+  | cnId : string -> constructorId.
+
+Inductive aliased { X : Type } : Type :=
+  | aliasOf (x : X).
+
+Inductive rhs : Type :=
+  | rhsPath (p : path)
+  | fieldAssign (pf : fieldOfPath) (ap : @aliased path)
+  | methodCall (rcvr : @aliased path) (m : methodId) (args : list (@aliased path))
+  | behaviourCall (rcvr : @aliased path) (b : behaviourId) (args : list (@aliased path))
+  | constructorCall (rcvrType : ponyType) (k : constructorId) (args : list (@aliased path)).
+
+Inductive expression : Type :=
+  | varDecl (x : var)
+  | assign (x : var) (arhs : @aliased rhs)
+  | tempAssign (t : Temporary) (pf : fieldOfPath).
+
+Inductive expressionSeq : Type :=
+  | final : path -> expressionSeq
+  | seq : expression -> expressionSeq -> expressionSeq.
+
+End Syntax.
+
+Require Import Coq.Structures.Equalities.
+
+Module DecidableVar.
+Include (UsualDecidableTypeBoth with Definition t := Syntax.var).
+Scheme Equality for Syntax.var.
+End DecidableVar.
+
+Module DecidableField.
+Include (UsualDecidableTypeBoth with Definition t := Syntax.fieldId).
+Scheme Equality for Syntax.fieldId.
+End DecidableField.
+
+Module DecidableConstructor.
+Include (UsualDecidableTypeBoth with Definition t := Syntax.constructorId).
+Scheme Equality for Syntax.constructorId.
+End DecidableConstructor.
+
+Module DecidableMethod.
+Include (UsualDecidableTypeBoth with Definition t := Syntax.methodId).
+Scheme Equality for Syntax.methodId.
+End DecidableMethod.
+
+Module DecidableBehaviour.
+Include (UsualDecidableTypeBoth with Definition t := Syntax.behaviourId).
+Scheme Equality for Syntax.behaviourId.
+End DecidableBehaviour.
+
+Module DecidableClass.
+Include (UsualDecidableTypeBoth with Definition t := Syntax.classId).
+Scheme Equality for Syntax.classId.
+End DecidableClass.
+
+Module DecidableActor.
+Include (UsualDecidableTypeBoth with Definition t := Syntax.actorId).
+Scheme Equality for Syntax.actorId.
+End DecidableActor.
+
+Require Import Coq.FSets.FMapInterface.
+
+Module Program (Map : WSfun).
+
+Module VarMap := Map DecidableVar.
+Definition varMap := VarMap.t.
+
+Module FieldMap := Map DecidableField.
+Definition fieldMap := FieldMap.t.
+
+Module ConstructorMap := Map DecidableConstructor.
+Definition constructorMap := ConstructorMap.t.
+
+Module MethodMap := Map DecidableMethod.
+Definition methodMap := MethodMap.t.
+
+Module BehaviourMap := Map DecidableBehaviour.
+Definition behaviourMap := BehaviourMap.t.
+
+Module ClassMap := Map DecidableClass.
+Definition classMap := ClassMap.t.
+
+Module ActorMap := Map DecidableActor.
+Definition actorMap := ActorMap.t.
+
+Inductive constructorDef : Type :=
+  | cnDef (args : varMap Syntax.aliasedType) (body : Syntax.expressionSeq).
+
+Inductive methodDef : Type :=
+  | mDef
+      (receiverCap : Syntax.baseCapability)
+      (args : varMap Syntax.aliasedType)
+      (returnType : Syntax.ponyType)
+      (body : Syntax.expressionSeq).
+
+Inductive behaviourDef : Type :=
+  | bDef (args : varMap Syntax.aliasedType) (body : Syntax.expressionSeq).
+
+Record classDef : Type :=
+  cDef
+  { classFields : fieldMap Syntax.aliasedType
+  ; classConstructors : constructorMap constructorDef
+  ; classMethods : methodMap methodDef
+  }.
+
+Record actorDef : Type :=
+  aDef 
+  { actorFields : fieldMap Syntax.aliasedType
+  ; actorConstructors : constructorMap constructorDef
+  ; actorMethods : methodMap methodDef
+  ; actorBehaviours : behaviourMap behaviourDef
+  }.
+
+Record program : Type :=
+  prog
+  { classes : classMap classDef
+  ; actors : actorMap actorDef
+  }.
+
+Definition fieldLookup { p : program } (s : Syntax.typeId) (f : Syntax.fieldId) (t : Syntax.aliasedType) : Prop
+  := exists (c : Syntax.classId) (cd : classDef), s = inl c /\ ClassMap.MapsTo c cd (classes p) /\ FieldMap.MapsTo f t (classFields cd)
+      \/ exists (a : Syntax.actorId) (ad : actorDef), s = inr a /\ ActorMap.MapsTo a ad (actors p) /\ FieldMap.MapsTo f t (actorFields ad).
+
+End Program.
---
+++ Typing.v
@@ -0,0 +1,83 @@
+Add LoadPath "." as Pony.
+From Pony Require Import Language.
+
+Require Import Coq.FSets.FMapInterface.
+Require Import Coq.Structures.Equalities.
+
+Module Typing (Map : WSfun).
+
+Module Program := Program Map.
+Include Program.
+Include Syntax.
+
+Definition viewAdapt
+  (objCap : { k : Syntax.capability | k <> Syntax.base Syntax.tag })
+  (fieldCap : Syntax.baseCapability) 
+  : Syntax.capability.
+Proof.
+  Admitted.
+
+Definition writeAdapt
+  (objCap : Syntax.capability)
+  (fieldCap : Syntax.baseCapability)
+  : Syntax.capability.
+Proof.
+  Admitted.
+
+Definition safeToWrite
+  (objCap : Syntax.capability)
+  (valCap : Syntax.baseCapability)
+  : Prop.
+Proof.
+  Admitted.
+
+Definition context : Type := (varMap Syntax.aliasedType).
+
+Definition sendableCtxt (gamma : context) : context :=
+  VarMap.fold
+    (fun var varType sendableMap =>
+      match varType with
+      | Syntax.aType _ b =>
+          if Syntax.isSendable b
+            then VarMap.add var varType sendableMap
+            else sendableMap
+      end)
+    (VarMap.empty Syntax.aliasedType)
+    gamma.
+ 
+Reserved Notation "g |-{ X } x : T ==> g'" (at level 9, x at level 50, T at level 50).
+
+Inductive typing { P : Program.program } : forall ( X : Type), context -> X -> Syntax.ponyType -> context -> Prop :=
+  (* Path rules *)
+  | path_var (gamma : context) (x : Syntax.var) (aT : Syntax.aliasedType) 
+  : VarMap.MapsTo x aT gamma 
+      -> gamma |-{ Syntax.path } (Syntax.use x) : Syntax.asPonyType aT ==> gamma
+  | path_consume (gamma : context) (x : Syntax.var) (aT : Syntax.aliasedType)
+  : VarMap.MapsTo x aT gamma
+      -> gamma |-{ Syntax.path } (Syntax.consume x) : Syntax.asPonyType aT ==> (VarMap.remove x gamma)
+  | path_field (gamma gamma' : context) (p : Syntax.path) (s s' : Syntax.typeId) (k : Syntax.capability) (k' : Syntax.baseCapability) (f : Syntax.fieldId) (witness_k_not_tag : k <> Syntax.base Syntax.tag)
+  : gamma |-{ Syntax.path } p : (Syntax.type s k) ==> gamma'
+      -> @Program.fieldLookup P s f (Syntax.aType s' k')
+      -> gamma |-{ Syntax.fieldOfPath } (p, f) : (Syntax.type s' (viewAdapt (exist (fun x => x <> Syntax.base Syntax.tag) k witness_k_not_tag) k')) ==> gamma'
+  (* The alias rule *)
+  | expr_alias { X : Type } (gamma gamma': context) (x : X) (s : Syntax.typeId) (k : Syntax.capability) (b : Syntax.baseCapability)
+  : gamma |-{ X } x : (Syntax.type s k) ==> gamma'
+    -> (Syntax.alias k <; Syntax.base b)
+    -> gamma |-{ Syntax.aliased } (Syntax.aliasOf x) : (Syntax.type s (Syntax.base b)) ==> gamma'
+  | expr_vardecl (gamma : context) (x : Syntax.var) (aT : Syntax.aliasedType)
+  : ~ VarMap.In x gamma
+      -> gamma |-{ Syntax.expression } Syntax.varDecl x : Syntax.asPonyType aT ==> (VarMap.add x aT gamma)
+  | expr_localassign (gamma gamma' : context) (x : Syntax.var) (arhs : @Syntax.aliased Syntax.rhs) (aT : Syntax.aliasedType)
+  : gamma |-{ @Syntax.aliased Syntax.rhs } arhs : Syntax.asPonyType aT ==> gamma
+    -> VarMap.MapsTo x aT gamma'
+    -> gamma |-{ Syntax.expression } Syntax.assign x arhs : Syntax.hat aT ==> gamma'
+  | expr_fieldassign (gamma gamma' gamma'' : context) (p p' : Syntax.path) (f : Syntax.fieldId) (s s' : Syntax.typeId) (k : Syntax.capability) (b b' : Syntax.baseCapability)
+  : gamma |-{ Syntax.aliased } (Syntax.aliasOf p') : (Syntax.type s' (Syntax.base b)) ==> gamma'
+      -> gamma' |-{ Syntax.path } p : (Syntax.type s k) ==> gamma''
+      -> @Program.fieldLookup P s f (Syntax.aType s' b')
+      -> safeToWrite k b
+      -> (Syntax.base b) <; (Syntax.base b')
+      -> gamma |-{ Syntax.rhs } Syntax.fieldAssign (p, f) (Syntax.aliasOf p') : Syntax.type s' (writeAdapt k b') ==> gamma''
+where "G |-{ X } x : T ==> G'" := (typing X G x T G').
+
+End Typing.
