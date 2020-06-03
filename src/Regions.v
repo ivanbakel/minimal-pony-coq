b5438cce84811c7b9d66088beaa3ea664aaad6d5
Isaac van Bakel <ivb@vanbakel.io>
2020-06-03 16:28:31 +0100

    Added full definition of well-formed frames

--- src/Regions.v
+++ src/Regions.v
@@ -137,25 +137,27 @@ Inductive well_formed_message { p : program } (chi : heap) (r : regPart chi) : o
   | wf_message_nul (a : Syntax.actorId)
   : well_formed_message chi r None a.
 
-Inductive well_formed_frame (chi : heap) (r : regPart chi) : option frameAddr -> Prop :=
-  (* TODO: This is not complete *)
-  | wf_frame_top (iota : frameAddr) (gamma : context) (L : localVars) (E : Syntax.expressionSeq) (v_super : frameAddr?)
+Inductive well_formed_frame { P : program } (chi : heap) (r : regPart chi) : option frameAddr -> Prop :=
+  | wf_frame_top (iota : frameAddr) (gamma : context) (L : localVars) (E : Syntax.expressionSeq)
+    (v_super : frameAddr?) (t : Syntax.ponyType)
   : HeapMapsTo frame (someFrameAddr iota) (frameAlloc L E None v_super) chi
-    -> False (* TODO: check local variables are well-typed w.r.t. something *)
-    -> False (* TODO: check the return type is good for the superframe *)
+    -> @well_formed_expr P gamma E t
+    -> well_typed_locals chi r (someFrameAddr iota) L gamma
+    -> well_formed_frame_if_returned chi r v_super t
     -> perspJudgement chi r (someFrameAddr iota) super (option_map someFrameAddr v_super) Syntax.iso
     -> well_formed_frame chi r (Some iota)
   | wf_frame_null
   : well_formed_frame chi r None
 with
-well_formed_frame_if_returned (chi : heap) (r : regPart chi) : option frameAddr -> Syntax.ponyType -> Prop :=
-  | wf_frame_ir (iota : frameAddr) (gamma : context) (L : localVars) (E : Syntax.expressionSeq) (v_super : frameAddr?)
-      (t : Syntax.ponyType)
-  : HeapMapsTo frame (someFrameAddr iota) (frameAlloc L E None v_super) chi
-    -> False (* TODO: check local variables are well-typed w.r.t. something *)
-    -> False (* TODO: check the return type is good for the superframe *)
+well_formed_frame_if_returned { P : program } (chi : heap) (r : regPart chi) : option frameAddr -> Syntax.ponyType -> Prop :=
+  | wf_frame_ir (iota : frameAddr) (gamma : context) (L : localVars) (E : Syntax.expressionSeq)
+    (v_super : frameAddr?) (t : Syntax.ponyType) (t' : Syntax.aliasedType) (y : Syntax.var)
+  : HeapMapsTo frame (someFrameAddr iota) (frameAlloc L E (Some y) v_super) chi
+    -> @well_formed_expr P (addVar y t' gamma) E t
+    -> well_typed_locals chi r (someFrameAddr iota) L gamma
+    -> well_formed_frame_if_returned chi r v_super t
     -> perspJudgement chi r (someFrameAddr iota) super (option_map someFrameAddr v_super) Syntax.iso
-    -> well_formed_frame_if_returned chi r (Some iota) t
+    -> well_formed_frame_if_returned chi r (Some iota) (Syntax.asPonyType t')
   | wf_frame_ir_no_return (v : option frameAddr) (t : Syntax.ponyType)
   : well_formed_frame chi r v 
     -> well_formed_frame_if_returned chi r v t.
@@ -175,7 +177,7 @@ Definition well_formed_actor { P : program } (chi : heap) (r : regPart chi) (iot
     /\ perspJudgement chi r (someActorAddr iota) (varAcc Syntax.this) (Some (someActorAddr iota)) Syntax.iso
     /\ @well_typed_fields P chi r (someActorAddr iota) F (actorTypeId a)
     /\ @well_formed_message P chi r v_mes a
-    /\ well_formed_frame chi r v_frm. 
+    /\ @well_formed_frame P chi r v_frm. 
 
 Definition well_formed_heap { P : program } (chi : heap) (r : regPart chi) : Prop :=
   ( forall iota_a : actorAddr,
